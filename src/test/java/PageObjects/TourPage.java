6880ca6ed6b6cc3018a55030f85b7324c91a4865
alex <alex92471@gmail.com>
2020-03-20 12:10:00 +0300

    Added element type to TourPage elements names. Fixes #5

--- src/test/java/PageObjects/TourPage.java
+++ src/test/java/PageObjects/TourPage.java
@@ -2,9 +2,6 @@ package PageObjects;
 
 import DataHelper.DataProcessor;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.conditions.Text;
-import lombok.Value;
-import lombok.val;
 
 import static DataHelper.DataProcessor.Card.getFakeCardForTest;
 import static com.codeborne.selenide.Condition.*;
@@ -16,22 +13,22 @@ public class TourPage {
     private static SelenideElement purchaseWithCardButton = $(byText("Купить"));
     private static SelenideElement purchaseWithLoanButton = $(byText("Купить в кредит"));
 
-    private static SelenideElement cardNumber = $("input[maxlength='19']");
-    private static SelenideElement cardMonthExpiration = $$("input[maxlength='2']").first();
-    private static SelenideElement cardYearExpiration = $$("input[maxlength='2']").last();
-    private static SelenideElement cardHolder = $(byText("Владелец")).parent().$("input");
-    private static SelenideElement cardCvcCvv = $("input[maxlength='3']");
+    private static SelenideElement cardNumberField = $("input[maxlength='19']");
+    private static SelenideElement cardMonthExpirationField = $$("input[maxlength='2']").first();
+    private static SelenideElement cardYearExpirationField = $$("input[maxlength='2']").last();
+    private static SelenideElement cardHolderField = $(byText("Владелец")).parent().$("input");
+    private static SelenideElement cardCvcCvvField = $("input[maxlength='3']");
 
     private static SelenideElement continueButton = $(withText("Продолжить"));
     private static SelenideElement waitingButton = $(withText("Отправляем запрос в Банк"));
 
 
     public static void clearForm() {
-        cardNumber.setValue("");
-        cardMonthExpiration.setValue("");
-        cardYearExpiration.setValue("");
-        cardHolder.setValue("");
-        cardCvcCvv.setValue("");
+        cardNumberField.setValue("");
+        cardMonthExpirationField.setValue("");
+        cardYearExpirationField.setValue("");
+        cardHolderField.setValue("");
+        cardCvcCvvField.setValue("");
     }
 
     public static void selectPurchaseWithCard() {
@@ -45,11 +42,11 @@ public class TourPage {
     }
 
     public static void sendFormWithValidCardData(DataProcessor.Card card) {
-        cardNumber.setValue(card.getCardNumber());
-        cardMonthExpiration.setValue(card.getCardMonthExpiration());
-        cardYearExpiration.setValue(card.getCardYearExpiration());
-        cardHolder.setValue(card.getCardHolder());
-        cardCvcCvv.setValue(card.getCardCvcCvv());
+        cardNumberField.setValue(card.getCardNumber());
+        cardMonthExpirationField.setValue(card.getCardMonthExpiration());
+        cardYearExpirationField.setValue(card.getCardYearExpiration());
+        cardHolderField.setValue(card.getCardHolder());
+        cardCvcCvvField.setValue(card.getCardCvcCvv());
         continueButton.click();
         continueButton.shouldBe(hidden);
         waitingButton.waitUntil(visible, 1000);
@@ -58,11 +55,11 @@ public class TourPage {
     }
 
     public static void sendFormWithInvalidCardData(DataProcessor.Card card) {
-        cardNumber.setValue(card.getCardNumber());
-        cardMonthExpiration.setValue(card.getCardMonthExpiration());
-        cardYearExpiration.setValue(card.getCardYearExpiration());
-        cardHolder.setValue(card.getCardHolder());
-        cardCvcCvv.setValue(card.getCardCvcCvv());
+        cardNumberField.setValue(card.getCardNumber());
+        cardMonthExpirationField.setValue(card.getCardMonthExpiration());
+        cardYearExpirationField.setValue(card.getCardYearExpiration());
+        cardHolderField.setValue(card.getCardHolder());
+        cardCvcCvvField.setValue(card.getCardCvcCvv());
         continueButton.click();
         continueButton.shouldBe(hidden);
         waitingButton.waitUntil(visible, 1000);
@@ -76,16 +73,16 @@ public class TourPage {
         continueButton.click();
         assertTrue(continueButton.getText().contains("Продолжить"));
 
-        cardNumber.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardNumber.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardMonthExpiration.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardMonthExpiration.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardYearExpiration.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardYearExpiration.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardHolder.parent().parent().$("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        cardHolder.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardCvcCvv.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardCvcCvv.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardNumberField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardNumberField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardMonthExpirationField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardMonthExpirationField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardYearExpirationField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardYearExpirationField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardHolderField.parent().parent().$("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        cardHolderField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardCvcCvvField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardCvcCvvField.parent().parent().$("span.input__sub").shouldBe(visible);
     }
 
     public static void sendBlankFormWithLoan() {
@@ -94,36 +91,36 @@ public class TourPage {
         continueButton.click();
         assertTrue(continueButton.getText().contains("Продолжить"));
 
-        cardNumber.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardNumber.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardMonthExpiration.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardMonthExpiration.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardYearExpiration.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardYearExpiration.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardHolder.parent().parent().$("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        cardHolder.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardCvcCvv.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardCvcCvv.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardNumberField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardNumberField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardMonthExpirationField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardMonthExpirationField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardYearExpirationField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardYearExpirationField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardHolderField.parent().parent().$("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        cardHolderField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardCvcCvvField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardCvcCvvField.parent().parent().$("span.input__sub").shouldBe(visible);
     }
 
     public static void sendFormWithBrokenData() {
         selectPurchaseWithCard();
         clearForm();
-        cardNumber.setValue(getFakeCardForTest().getCardNumber());
-        cardMonthExpiration.setValue(getFakeCardForTest().getCardMonthExpiration());
-        cardYearExpiration.setValue(getFakeCardForTest().getCardYearExpiration());
-        cardHolder.setValue(getFakeCardForTest().getCardHolder());
-        cardCvcCvv.setValue(getFakeCardForTest().getCardCvcCvv());
+        cardNumberField.setValue(getFakeCardForTest().getCardNumber());
+        cardMonthExpirationField.setValue(getFakeCardForTest().getCardMonthExpiration());
+        cardYearExpirationField.setValue(getFakeCardForTest().getCardYearExpiration());
+        cardHolderField.setValue(getFakeCardForTest().getCardHolder());
+        cardCvcCvvField.setValue(getFakeCardForTest().getCardCvcCvv());
         continueButton.click();
         assertTrue(continueButton.getText().contains("Продолжить"));
 
-        cardNumber.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardNumber.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardYearExpiration.parent().parent().$("span.input__sub").shouldHave(exactText("Истёк срок действия карты"));
-        cardYearExpiration.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardHolder.parent().parent().$("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
-        cardHolder.parent().parent().$("span.input__sub").shouldBe(visible);
-        cardCvcCvv.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
-        cardCvcCvv.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardNumberField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardNumberField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardYearExpirationField.parent().parent().$("span.input__sub").shouldHave(exactText("Истёк срок действия карты"));
+        cardYearExpirationField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardHolderField.parent().parent().$("span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        cardHolderField.parent().parent().$("span.input__sub").shouldBe(visible);
+        cardCvcCvvField.parent().parent().$("span.input__sub").shouldHave(exactText("Неверный формат"));
+        cardCvcCvvField.parent().parent().$("span.input__sub").shouldBe(visible);
     }
 }
