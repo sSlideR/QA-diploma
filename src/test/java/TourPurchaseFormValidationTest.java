2318f7b20a97c878a1591a293758094dfd498b6c
alex <alex92471@gmail.com>
2020-03-29 16:48:32 +0300

    Additional form validation tests added. Fixes #12

---
+++ src/test/java/TourPurchaseFormValidationTest.java
@@ -0,0 +1,138 @@
+import DataHelper.DataProcessor;
+import DataHelper.SqlUtils.DataBase;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.Data;
+import lombok.val;
+import org.junit.jupiter.api.*;
+
+import java.sql.SQLException;
+
+import static DataHelper.SqlUtils.DataBase.OrderEntityItem.getLastDbItemFromOrderEntity;
+import static DataHelper.SqlUtils.DataBase.assertNewOrderIsNotRecordedInDb;
+import static DataHelper.SqlUtils.DataBase.assertNewOrderIsRecordedInDb;
+import static PageObjects.TourPage.*;
+import static com.codeborne.selenide.Selenide.open;
+
+@DisplayName("Form validation")
+@Data
+public class TourPurchaseFormValidationTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void openBrowser() {
+        String sutUrl = (System.getProperty("sut.url") == null || "".equals(System.getProperty("sut.url"))) ? "http://localhost:8080" : System.getProperty("sut.url");
+        open(sutUrl);
+    }
+
+    @Test
+    void blankFormShouldntBeSendWithCard() throws SQLException {
+        DataBase.OrderEntityItem lastOrder = getLastDbItemFromOrderEntity();
+
+        selectPurchaseWithCard();
+        clearForm();
+        submitForm();
+
+        assertButtonStateNotChangedWithErrorsInForm();
+        assertNewOrderIsNotRecordedInDb(lastOrder);
+        assertInvalidFormatErrorVisibleForCardExpirationFieldIfValid();
+        assertInvalidFormatErrorVisibleForCardHolderField();
+        assertInvalidFormatErrorVisibleForCardCvcCvvField();
+        assertInvalidFormatErrorVisibleForCardNumberField();
+        assertInvalidFormatErrorVisibleForCardCvcCvvField();
+    }
+
+    @Test
+    void blankFormShouldntBeSendWithLoan() throws SQLException {
+        DataBase.OrderEntityItem lastOrder = getLastDbItemFromOrderEntity();
+
+        selectPurchaseWithLoan();
+        clearForm();
+        submitForm();
+
+        assertButtonStateNotChangedWithErrorsInForm();
+        assertNewOrderIsNotRecordedInDb(lastOrder);
+        assertInvalidFormatErrorVisibleForCardExpirationFieldIfValid();
+        assertInvalidFormatErrorVisibleForCardHolderField();
+        assertInvalidFormatErrorVisibleForCardCvcCvvField();
+        assertInvalidFormatErrorVisibleForCardNumberField();
+        assertInvalidFormatErrorVisibleForCardCvcCvvField();
+    }
+
+    @Test
+    void errorNotificationShouldBeVisibleIfShortenedCardNumberInsertedInCardNumberField() {
+        DataProcessor.Card validCardForTest = DataProcessor.Card.getValidCardForTest();
+        DataProcessor.Card brokenCardForTest = DataProcessor.Card.CardFaker.getCardWithShortenedCardNumber();
+
+        selectPurchaseWithCard();
+        fillCardNumberField(brokenCardForTest);
+        fillCardMonthExpirationField(validCardForTest);
+        fillCardYearExpirationField(validCardForTest);
+        fillCardHolderField(validCardForTest);
+        fillCardCvcCvvField(validCardForTest);
+        submitForm();
+
+        assertInvalidFormatErrorVisibleForCardNumberField();
+        assertButtonStateNotChangedWithErrorsInForm();
+    }
+
+    @Test
+    void errorNotificationShouldBeVisibleIfExpiredCardDueDatesInsertedInExpirationFields() {
+        DataProcessor.Card validCardForTest = DataProcessor.Card.getValidCardForTest();
+        DataProcessor.Card brokenCardForTest = DataProcessor.Card.CardFaker.getCardWithExpiredDates();
+
+        selectPurchaseWithCard();
+        fillCardNumberField(validCardForTest);
+        fillCardMonthExpirationField(brokenCardForTest);
+        fillCardYearExpirationField(brokenCardForTest);
+        fillCardHolderField(validCardForTest);
+        fillCardCvcCvvField(validCardForTest);
+        submitForm();
+
+        assertInvalidFormatErrorVisibleForCardExpirationFieldIfExpired();
+        assertButtonStateNotChangedWithErrorsInForm();
+    }
+
+    @Test
+    void errorNotificationShouldBeVisibleIfZeroDatesInsertedInExpirationFields() {
+        DataProcessor.Card validCardForTest = DataProcessor.Card.getValidCardForTest();
+        DataProcessor.Card brokenCardForTest = DataProcessor.Card.CardFaker.getCardWithZeroDates();
+
+        selectPurchaseWithCard();
+        fillCardNumberField(validCardForTest);
+        fillCardMonthExpirationField(brokenCardForTest);
+        fillCardYearExpirationField(brokenCardForTest);
+        fillCardHolderField(validCardForTest);
+        fillCardCvcCvvField(validCardForTest);
+        submitForm();
+
+        assertInvalidFormatErrorVisibleForCardExpirationFieldIfValid();
+        assertButtonStateNotChangedWithErrorsInForm();
+    }
+
+    @Test
+    void errorNotificationShouldBeVisibleIfShortenedCvcCvvInsertedInCardCvcCvvField() {
+        DataProcessor.Card validCardForTest = DataProcessor.Card.getValidCardForTest();
+        DataProcessor.Card brokenCardForTest = DataProcessor.Card.CardFaker.getCardWithShortenedCvcCvv();
+
+        selectPurchaseWithCard();
+        fillCardNumberField(validCardForTest);
+        fillCardMonthExpirationField(validCardForTest);
+        fillCardYearExpirationField(validCardForTest);
+        fillCardHolderField(validCardForTest);
+        fillCardCvcCvvField(brokenCardForTest);
+        submitForm();
+
+        assertInvalidFormatErrorVisibleForCardCvcCvvField();
+        assertButtonStateNotChangedWithErrorsInForm();
+    }
+
+}
