573419b6616d022885e5c4ece0004376cbce2d79
alex <alex92471@gmail.com>
2020-04-05 12:24:51 +0300

    Added overrided equals and hashCode methods for OrderEntity

--- src/test/java/DataHelper/SqlUtils/OrderEntity.java
+++ src/test/java/DataHelper/SqlUtils/OrderEntity.java
@@ -2,10 +2,28 @@ package DataHelper.SqlUtils;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
-class OrderEntity {
+public class OrderEntity {
     private String id;
     private String created;
     private String credit_id;
     private String payment_id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntity that = (OrderEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(created, that.created) &&
+                Objects.equals(credit_id, that.credit_id) &&
+                Objects.equals(payment_id, that.payment_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, created, credit_id, payment_id);
+    }
 }
